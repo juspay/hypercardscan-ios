d0c16e056f6d18e17fa9dc8c8590f680c5b2b7ed
Balaganesh S <balaganesh@juspay.in>
2026-06-04 14:22:34 +0530

    Updated Package for version 2.2.7.8

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "HyperCardScan",
-            url: "https://public.releases.juspay.in/release/ios/hyper-sdk/2.2.7.7/HyperCardScan.zip",
-            checksum: "f5545190cbfbf9bd46efc4bb064ba84fbb8d02d428e4ff9fbb8f6288ad92550a"
+            url: "https://public.releases.juspay.in/release/ios/hyper-sdk/2.2.7.8/HyperCardScan.zip",
+            checksum: "7353c2e5177b43bd8de3444ca281b48d370460ca229c1fd682dc5468f4da3d37"
         )
     ]
 )
